0ba5535a1a78ad368035ff7d5ac91fbd0e5ed74f
Sergey Pedan <sergey.pedan@gmail.com>
2019-08-26 04:56:25 +0300

    Corrects load paths

--- lib/jaimini/course_certificates.rb
+++ lib/jaimini/course_certificates.rb
@@ -2,10 +2,10 @@
 
 require "dry/schema"
 
-require_relative "course_certificates/version"
-require_relative "course_certificates/record"
-require_relative "course_certificates/certificate"
-require_relative "course_certificates/course"
-require_relative "course_certificates/user"
-require_relative "course_certificates/pdf_composer"
-require_relative "course_certificates/pdf_builder"
+require "jaimini/course_certificates/version"
+require "jaimini/course_certificates/record"
+require "jaimini/course_certificates/certificate"
+require "jaimini/course_certificates/course"
+require "jaimini/course_certificates/user"
+require "jaimini/course_certificates/pdf_composer"
+require "jaimini/course_certificates/pdf_builder"
--- lib/jaimini/course_certificates/helpers.rb
+++
@@ -1,9 +0,0 @@
-# module Jaimini
-#   module Helpers
-
-#     def symbolize_keys(hash)
-#       hash.transform_keys { |key| key.to_sym rescue key }
-#     end
-
-#   end
-# end
